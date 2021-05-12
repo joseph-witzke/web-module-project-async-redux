116beb30a26aaf1eb50fc4bd6042ef829e538ef1
Joe Witzke <joewitzke@joes-mbp.lan>
2021-05-12 14:18:42 -0600

    axios call made and returning data

--- app/src/components/Cat.js
+++ app/src/components/Cat.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 const Cat = () => {
+    axios.get('https://randomuser.me/api/')
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        });
     return (
         <div className="container">
             <h2>Yo here is a cat:</h2>
